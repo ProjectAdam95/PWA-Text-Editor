c401836b93aabc84cc2e67cc87c2ba5672800ef0
Adam Todorovic <a.tod95@hotmail.com>
2024-10-14 09:53:49 +1100

    Updated package.json for Render deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,29 +5,26 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development', // Can change to 'production' for the final build
-    
-    // Set context to ensure the correct base directory is used
-    context: path.resolve(__dirname, 'client'),  // Set client directory as context
+    mode: 'production', // Change to production for final builds
 
     entry: {
-      main: './src/js/index.js',     // Path relative to 'client/'
-      install: './src/js/install.js' // Path relative to 'client/'
+      main: './src/js/index.js',  // Correct path relative to 'client'
+      install: './src/js/install.js',  // Correct path relative to 'client'
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'client/dist'),
+      path: path.resolve(__dirname, 'dist'),  // Ensure correct output path
     },
     plugins: [
       // Generates an HTML file that includes your bundles
       new HtmlWebpackPlugin({
-        template: './index.html', // Path relative to 'client/'
+        template: './index.html',  // Path relative to 'client'
         title: 'Text Editor PWA',
       }),
 
       // Injects the custom service worker file
       new InjectManifest({
-        swSrc: './src-sw.js', // Path relative to 'client/'
+        swSrc: './src-sw.js',  // Correct path relative to 'client'
         swDest: 'service-worker.js',
       }),
 
@@ -44,13 +41,14 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'), // Path relative to 'client/'
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            src: path.resolve(__dirname, 'src/images/logo.png'),  // Corrected path
+            sizes: [96, 128, 192, 256, 384, 512],  // Icon sizes
+            destination: path.join('assets', 'icons'),  // Output folder
           },
         ],
       }),
     ],
+
     module: {
       rules: [
         // CSS loaders
